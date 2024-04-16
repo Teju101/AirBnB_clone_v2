7cfbcc408e9f1fd20f8c586d2a16ed4fc614c224
daadesina <daadesina1@gmail.com>
2024-04-16 08:42:28 +0100

    adeteju

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---MySQL setup development
+-- prepares a MySQL server for the AirBnB clone
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
